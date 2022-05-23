c2c12108712a14cf3fb04ce7442bc87854a551b4
OusmanNdaw <105175511+OusmanNdaw@users.noreply.github.com>
2022-05-23 13:53:09 +0200

    Ovning3aUpdate with Animal class

--- Animal.cs
+++ Animal.cs
@@ -4,6 +4,12 @@ public class Animal
 {
 	public Animal()
 	{
-
+		Animal new Animal();
+		Animal Horse = new Animal();
+		Animal Dog = new Animal();
+		Animal HedgeHog = new Animal();
+		Animal Worm = new Animal();
+		Animal Bird = new Animal();
+		Animal Wolf = new Animal();
 	}
 }
