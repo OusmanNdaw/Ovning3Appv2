9d3db3e28b57f234ea7c5e4ae70b2cf7366bce65
OusmanNdaw <105175511+OusmanNdaw@users.noreply.github.com>
2022-05-23 13:35:38 +0200

    Ovning3a update

---
+++ PersonHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class PersonHandler
+{
+	public PersonHandler()
+	{
+		public Person CreatePerson(int age, string fname,
+		string lname, double height, double weight)
+	}
+}
